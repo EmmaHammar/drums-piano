1c412fc05761705f6699e2f695edde4096153a3f
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-02-03 11:30:05 +0100

    adds sound for each drum click

--- script.js
+++ script.js
@@ -16,40 +16,63 @@ console.log(drumItem2);
 console.log("drumItem", drumItem);
 
 
+// const sound = document.getElementById("soundDrumClap");
+// sound.play();   
+
+
+
 drumItem1.addEventListener("click", function() {
     console.log("klick1");
+    let sound1 = document.getElementById("sound1");
+    sound1.play(); 
 });
 
 drumItem2.addEventListener("click", function() {
     console.log("klick2");
+    let sound2 = document.getElementById("sound2");
+    sound2.play(); 
 });
 
 drumItem3.addEventListener("click", function() {
     console.log("klick3");
+    let sound3 = document.getElementById("sound3");
+    sound3.play(); 
 });
 
 drumItem4.addEventListener("click", function() {
     console.log("klick4");
+    let sound4 = document.getElementById("sound4");
+    sound4.play(); 
 });
 
 drumItem5.addEventListener("click", function() {
     console.log("klick5");
+    let sound5 = document.getElementById("sound5");
+    sound5.play(); 
 });
 
 drumItem6.addEventListener("click", function() {
     console.log("klick6");
+    let sound6 = document.getElementById("sound6");
+    sound6.play(); 
 });
 
 drumItem7.addEventListener("click", function() {
     console.log("klick7");
+    let sound7 = document.getElementById("sound7");
+    sound7.play(); 
 });
 
 drumItem8.addEventListener("click", function() {
     console.log("klick8");
+    let sound8 = document.getElementById("sound8");
+    sound8.play(); 
 });
 
 drumItem9.addEventListener("click", function() {
     console.log("klick9");
+    let sound9 = document.getElementById("sound9");
+    sound9.play(); 
 });
 // document.addEventListener("keydown", function(evt){
 //     if (evt.key == "1") {
