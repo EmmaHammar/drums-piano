6c4a7c6b8ff52567133dfaf108c241bf65ad46e7
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-02-05 22:40:29 +0100

    adds drum sound when press 1-9 and rewrite code for play drum sound when click

--- script.js
+++ script.js
@@ -1,145 +1,165 @@
 const drumContainer = document.querySelector(".drumContainer");
 
-//Varför behöver vi ha både queryselector o getelementbyid?
-const drumItem = document.querySelector(".drumItem");
-const drumItem1 = document.getElementById("1");
-const drumItem2 = document.getElementById("2");
-const drumItem3 = document.getElementById("3");
-const drumItem4 = document.getElementById("4");
-const drumItem5 = document.getElementById("5");
-const drumItem6 = document.getElementById("6");
-const drumItem7 = document.getElementById("7");
-const drumItem8 = document.getElementById("8");
-const drumItem9 = document.getElementById("9");
+//alla divar med class="drumItem"
+const drumItems = document.querySelectorAll("div.drumItem");
 
-// console.log(drumItem2);
-// console.log("drumItem", drumItem);
+//HUR ÄNDRA BAKGRUND NÄR DU TRYCKER 1 för box 1?
+//play drums when press 1-9 keys
+document.addEventListener("keydown", function(evt) {
+    if (evt.key == "1") {
+        sound1.play(); 
+    } 
+    if (evt.key == "2") {
+        sound2.play(); 
+    }
+    if (evt.key == "3") {
+        sound3.play(); 
+    }
+    if (evt.key == "4") {
+        sound4.play(); 
+    }
+    if (evt.key == "5") {
+        sound5.play(); 
+    }
+    if (evt.key == "6") {
+        sound6.play(); 
+    }
+    if (evt.key == "7") {
+        sound7.play(); 
+    }
+    if (evt.key == "8") {
+        sound8.play(); 
+    }
+    if (evt.key == "9") {
+        sound9.play(); 
+    }
+});
 
-drumItem1.addEventListener("click", function(e) {
-    console.log("klick1");
-    let sound1 = document.getElementById("sound1");
-    sound1.play(); 
-    //Change color when clicked on
-    e.target.style.backgroundColor = "darkSalmon";
-    //Change back to yellow after a while
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
+//play drums when click
+document.addEventListener("click", function(clickEvent) {
+    console.log(clickEvent);
+    console.log("clickEvent.target", clickEvent.target.id);
 
-drumItem2.addEventListener("click", function(e) {
-    console.log("klick2");
-    let sound2 = document.getElementById("sound2");
-    sound2.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
+    //change background when click
+    function changeColor() {
+        clickEvent.target.style.backgroundColor = "darkSalmon";
+        setTimeout(function() {
+            clickEvent.target.style.backgroundColor = "";
+        }, 100);
+    };
 
-drumItem3.addEventListener("click", function(e) {
-    console.log("klick3");
-    let sound3 = document.getElementById("sound3");
-    sound3.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
+    if (clickEvent.target.id == 1) {
+        sound1.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 2) {
+        sound2.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 3) {
+        sound3.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 4) {
+        sound4.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 5) {
+        sound5.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 6) {
+        sound6.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 7) {
+        sound7.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 8) {
+        sound8.play();
+        changeColor();
+    }
+    if (clickEvent.target.id == 9) {
+        sound9.play();
+        changeColor();
+    }
+})
 
-
-drumItem4.addEventListener("click", function(e) {
-    console.log("klick4");
-    let sound4 = document.getElementById("sound4");
-    sound4.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
-
-drumItem5.addEventListener("click", function(e) {
-    console.log("klick5");
-    let sound5 = document.getElementById("sound5");
-    sound5.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
-
-drumItem6.addEventListener("click", function(e) {
-    console.log("klick6");
-    let sound6 = document.getElementById("sound6");
-    sound6.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
-
-drumItem7.addEventListener("click", function(e) {
-    console.log("klick7");
-    let sound7 = document.getElementById("sound7");
-    sound7.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
-
-drumItem8.addEventListener("click", function(e) {
-    console.log("klick8");
-    let sound8 = document.getElementById("sound8");
-    sound8.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
-
-drumItem9.addEventListener("click", function(e) {
-    console.log("klick9");
-    let sound9 = document.getElementById("sound9");
-    sound9.play(); 
-    e.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        e.target.style.backgroundColor = "";
-    }, 100);
-}, false);
 
 //================================
 //============= PIANO  =========== 
 //================================
+
+//ljudet
+let pianoSound1 = document.getElementById("pianoSound1");
+let pianoSound2 = document.getElementById("pianoSound2");
+
+//li ljudet
+let liPianoSound1 = document.getElementById("liPianoSound1");
+let liPianoSound2 = document.getElementById("liPianoSound2");
+
+
+//elementet li har eventListener
+liPianoSound1.addEventListener("mouseover", function() {
+    pianoSound1.play();
+    liPianoSound1.style.backgroundColor = "orange";
+    setTimeout(function() {
+        liPianoSound1.style.backgroundColor = "";
+    }, 200);
+})
+
+liPianoSound2.addEventListener("mouseover", function() {
+    pianoSound2.play();
+    liPianoSound2.style.backgroundColor = "orange";
+    setTimeout(function() {
+        liPianoSound2.style.backgroundColor = "";
+    }, 200);
+})
+
+
+
+/*
 const pianoContainer = document.querySelector(".pianoContainer");
 // console.log("pianoContainer", pianoContainer);
 const pianoItems = document.querySelectorAll(".pianoItem");
 
-let pianoArray = [pianoSound1, pianoSound2, pianoSound3]
+let pianoArray = [pianoSound1, pianoSound2, pianoSound3, pianoSound4, pianoSound5, pianoSound6, pianoSound7];
 
 pianoContainer.addEventListener("mouseover", function(event) {
     event.target.style.backgroundColor = "orange";
+
+    //Vill koppla till respektive tangent
+    document.getElementById("pianoSound1").play();
+
 //    reset the color after a short delay
    setTimeout(function() {
     event.target.style.backgroundColor = "";
   }, 200);
+}); 
+*/
 
 //VILL EJ ATT DE SKA SPELAS SAMTIDIGT DOCK
-  for (i=0; i<pianoArray.length; i++) {
-    // console.log(pianoArray[i])
-    if(pianoArray[0]) {
-        console.log("ljud 1");
-    }
-    if(pianoArray[1]) {
-        console.log("ljud 2");
-        break;
-    }
-     
-}
+// for (i=0; i<pianoArray.length; i++) {
+//     console.log(pianoArray[i])
 
-}, false); 
+//     pianoArray[0].addEventListener("mouseover",      function(event1) {
+//         console.log("mouseover pianoarray0");
+//         event1.target.style.backgroundColor = "pink";
+//     })
+// };
 
 
+        // if(pianoArray[0]) {
+        //     console.log("ljud 1");
+        //     document.getElementById("pianoSound1");
+        //     pianoSound1.play();
+        // }
+        // if(pianoArray[1]) {
+        //     console.log("ljud 2");
+        //     break;
+        // }
+        // };
 
 
 // document.addEventListener("keydown", function(evt){
