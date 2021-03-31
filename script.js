bc572a89a330dd03e33e4aa41914e15e09e3d3cc
Emma Hammar <emmahammar@Emmas-MacBook-2.local>
2021-03-31 11:43:38 +0200

    adds changeColor function to pianoItems when keydown and mouseover

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const drumContainer = document.querySelector(".drumContainer");
-//alla divar med class="drumItem"
 const drumItems = document.querySelectorAll("div.drumItem");
 
-
-//play drums when press 1-9 keys
+//play drum sound when press key + change color
 document.addEventListener("keydown", function(evt) {
     // console.log("alla drumItems", evt.target.childNodes[3].childNodes);
+
     switch (evt.key) {
         case "1":
             sound1.play(); 
@@ -46,186 +45,145 @@ document.addEventListener("keydown", function(evt) {
     }
 });
 
-//ändra färg på keydown med css-klass
-function changeColorKey (element) {
-    element.classList.toggle("changeColorKey")
-      setTimeout( function() {
-         element.classList.toggle("changeColorKey")
-      },150)
-   }
-
-//play drums when click
+//play drum sound when click + change color
 document.addEventListener("click", function(clickEvent) {
     // console.log("varje drumItem vid klick", clickEvent.target);
-    changeColorKey(clickEvent.target);
 
     switch (clickEvent.target.id) {
         case "drumItem1":
             sound1.play();
-           
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem2":
             sound2.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem3":
             sound3.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem4":
             sound4.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem5":
             sound5.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem6":
             sound6.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem7":
             sound7.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem8":
             sound8.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
+
         case "drumItem9":
             sound9.play();
-            break;
+            changeColorKey(clickEvent.target);
+        break;
     };
 });
 
 //============= PIANO  =========== 
 const pianoContainer = document.querySelector("#pianoContainer");
-
-//alla divar med class="pianoItem"
 const pianoItems = document.querySelectorAll("div.pianoItem"); 
 
-//piano sound when clicked keys a-j
+//play piano sound when click key + change color
 document.addEventListener("keydown", function(evt) {
-    // console.log("evt.key", evt.key);
+    // console.log("pianoItems:", evt.target.childNodes[11].childNodes);
     switch (evt.key){
         case "a": 
             pianoSound1.play();
-            pianoItem1.classList.add("changeColorKey"); 
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[1]);
         break;
         case "s": 
             pianoSound2.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.add("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[3]);
         break;
         case "d": 
             pianoSound3.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.add("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[5]);
         break;
         case ("f"):
             pianoSound4.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.add("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
-        break;
-        case ("f"):
-            pianoSound4.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.add("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[7]);
         break;
         case ("g"):
             pianoSound5.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.add("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[9]);
         break;
         case ("h"):
             pianoSound6.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.add("changeColorKey");
-            pianoItem7.classList.remove("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[11]);
         break;
         case ("j"):
             pianoSound7.play();
-            pianoItem1.classList.remove("changeColorKey");
-            pianoItem2.classList.remove("changeColorKey");
-            pianoItem3.classList.remove("changeColorKey");
-            pianoItem4.classList.remove("changeColorKey");
-            pianoItem5.classList.remove("changeColorKey");
-            pianoItem6.classList.remove("changeColorKey");
-            pianoItem7.classList.add("changeColorKey");
+            changeColorKey(evt.target.childNodes[11].childNodes[13]);
         break;
     }
 });
 
-//piano sound when mouseover
+//play piano sound when mouseover + change color
 document.addEventListener("mouseover", function(e) {
-    console.log(e.target.id);
+    
     if (e.target.id == "pianoItem1") {
-        // pianoSound1.currentTime = 0; 
+        // console.log(document.querySelector("#pianoItem1"));
+        pianoSound1.currentTime = 0; 
         pianoSound1.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem1"));
     }
     if (e.target.id == "pianoItem2") {
+        pianoSound2.currentTime = 0; 
         pianoSound2.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem2"));
     }
     if (e.target.id == "pianoItem3") {
+        pianoSound3.currentTime = 0; 
         pianoSound3.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem3"));
     }
     if (e.target.id == "pianoItem4") {
+        pianoSound4.currentTime = 0; 
         pianoSound4.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem4"));
     }
     if (e.target.id == "pianoItem5") {
+        pianoSound5.currentTime = 0; 
         pianoSound5.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem5"));
     }
     if (e.target.id == "pianoItem6") {
+        pianoSound6.currentTime = 0; 
         pianoSound6.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem6"));
     }
     if (e.target.id == "pianoItem7") {
+        pianoSound7.currentTime = 0; 
         pianoSound7.play();
-        changeColor(e);
+        changeColorKey(document.querySelector("#pianoItem7"));
     }
 });
 
 
-//change key color when mouseover
-function changeColor(event) {
-    event.target.style.backgroundColor = "darkSalmon";
-    setTimeout(function() {
-        event.target.style.backgroundColor = "";
-    }, 300);
+//change color with css class
+function changeColorKey (element) {
+    element.classList.toggle("changeColorKey");
+
+    //change back to default color
+    setTimeout( function() {
+        element.classList.toggle("changeColorKey")
+    }, 150);
 };
-
-
-//KVAR: HUR ÄNDRA FÄRG NÄR KEYDOWN genom funktionen changeColor??
